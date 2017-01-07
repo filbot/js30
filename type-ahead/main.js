b0a79d0bd2b68d70d2d4b5afd10b5dcff3687c58
Filip Adamczyk <Filip.Adamczyk@icrossing.com>
2017-01-07 15:29:50 -0800

    Configure fetching of data and filter function

--- type-ahead/main.js
+++ type-ahead/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const endpoint = 'https://gist.githubusercontent.com/filbot/22a4823e9abf463929420cd7618d4d86/raw/355e86b9c478169895ff2938f022ef4b8b2504bb/cities.json';
+
+const citiesArray = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => citiesArray.push(...data))
+
+function findMatches(wordToMatch, citiesArray) {
+  return citiesArray.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.name.match(regex) || place.state.match(regex);
+  })
+}

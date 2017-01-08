0e6e3876473c6490bc576608276fff5cf79f4567
Filip Adamczyk <Filip.Adamczyk@icrossing.com>
2017-01-07 17:00:29 -0800

    Setup fetch for flight data

--- flight-tracker/main.js
+++ flight-tracker/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const endpoint = 'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=34.045648&lng=-118.232409&fDstL=0&fDstU=10';
+var myHeaders = new Headers();
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+const flightDataArray = [];
+
+
+fetch(flightDataArray, myInit).then(function(response) {
+  if(response.ok) {
+    response.blob().then(function(myBlob) {
+      flightDataArray.push(...myBlob);
+    });
+  } else {
+    console.log('Network response was not ok.');
+  }
+})
+.catch(function(error) {
+  console.log('There has been a problem with your fetch operation: ' + error.message);
+});
+
+
